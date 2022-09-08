d67932ad798a2111d7c664f71dd223a31f0748a3
MauricioCorzo <mauricio.corzo@yahoo.com>
2022-09-08 13:00:38 -0300

    Filtros combinados

--- client/src/Components/Cards.jsx
+++ client/src/Components/Cards.jsx
@@ -13,8 +13,12 @@ export const Cards = () => {
     const [buscador , setBuscador] = useState("")
     const [paginaActual, setPaginaActual] = useState(1)
     const [paisesXPagina] = useState(10)
-    const [filtro, setFiltro] = useState({filtro:""})
     const [cargador, setCargador] =useState(false)
+    const [datosBusqueda, setDatosBusqueda] = useState({
+        ordenamiento: "",
+        continente: "",
+        actividad: ""
+    })
     
     const dispatch = useDispatch()
     const paises = useSelector(state => state.paises)
@@ -25,77 +29,129 @@ export const Cards = () => {
         setCargador(true)
         setTimeout(()=>{
             setCargador(false)
-        },1800)          
-    },[])
-    console.log(paises)
-    console.log(actividades)
+        },1800)
+        filtroDePaises()
+        console.log(datosBusqueda)          
+        console.log(buscador)          
+    },[datosBusqueda])
+    // console.log(paises)
+    // console.log(actividades)
 
     const ordenar = (e) => {
         e.preventDefault()
-        setFiltro({[e.target.name]: e.target.value})
+        setDatosBusqueda({...datosBusqueda,[e.target.name]: e.target.value})
     }
 
      // Traer cierta cantidad de Paises    
     const ultimoPais = paginaActual * paisesXPagina
     const primerPais = ultimoPais - paisesXPagina
 
-    const mapeoDePaises = () => {
-        if(buscador){
-            const resultado = paises.filter(p =>  p.nombre.slice(0, buscador.length).toLowerCase() === buscador.toLowerCase().trim())
-            return resultado.slice(primerPais,ultimoPais)
-        } 
-        if(filtro.filtro.length === 1) {          
-            if (filtro.filtro === "B"){
-                const a_z = paises.sort((a,b) => a.nombre.localeCompare(b.nombre, 'en') )
-                return a_z.slice(primerPais,ultimoPais)
-            }
-            if(filtro.filtro === "C"){
-                const z_a = paises.sort((a,b) => b.nombre.localeCompare(a.nombre, 'en'))
-                return z_a.slice(primerPais,ultimoPais)
-            }
-            if(filtro.filtro === "D"){
-                const menorAmayor = paises.sort((a,b) => a.poblacion - b.poblacion)
-                return menorAmayor.slice(primerPais,ultimoPais)
-            }
-            if(filtro.filtro === "E"){
-                const mayorAmenor = paises.sort((a,b) => b.poblacion - a.poblacion)
-                return mayorAmenor.slice(primerPais,ultimoPais)
-            }
-            if(filtro.filtro === "F"){
-                const america = paises.filter(p => p.continente === "Americas")
-                return america.slice(primerPais,ultimoPais)
-            }
-            if(filtro.filtro === "G"){
-                const europa = paises.filter(p => p.continente === "Europe")
-                return europa.slice(primerPais,ultimoPais)
-            }
-            if(filtro.filtro === "H"){
-                const africa = paises.filter(p => p.continente === "Africa")
-                return africa.slice(primerPais,ultimoPais)
-            }
-            if(filtro.filtro === "I"){
-                const asia = paises.filter(p => p.continente === "Asia")
-                return asia.slice(primerPais,ultimoPais)
-            }
-            if(filtro.filtro === "J"){
-                const oceania = paises.filter(p => p.continente === "Oceania")
-                return oceania.slice(primerPais,ultimoPais)
-            }
-            if(filtro.filtro === "K"){
-                const antartida = paises.filter(p => p.continente === "Antarctic")
-                return antartida.slice(primerPais,ultimoPais)
-            }           
+    const paisesnormales = paises.slice(primerPais,ultimoPais)
+
+     const filtroDePaises = () => {
+        const resultado = filtrarOrdenamiento(paises).filter( filtrarContinente ).filter( filtrarActividades ).filter ( filtrarBuscador )
+        return resultado.slice(primerPais,ultimoPais)
+//         if(buscador){
+//             const resultado = paises.filter(p =>  p.nombre.slice(0, buscador.length).toLowerCase() === buscador.toLowerCase().trim())
+//             return resultado.slice(primerPais,ultimoPais)
+//         } 
+//         if(filtro.filtro.length === 1) {          
+//             if (filtro.filtro === "B"){
+//                 const a_z = paises.sort((a,b) => a.nombre.localeCompare(b.nombre, 'en') )
+//                 return a_z.slice(primerPais,ultimoPais)
+//             }
+//             if(filtro.filtro === "C"){
+//                 const z_a = paises.sort((a,b) => b.nombre.localeCompare(a.nombre, 'en'))
+//                 return z_a.slice(primerPais,ultimoPais)
+//             }
+//             if(filtro.filtro === "D"){
+//                 const menorAmayor = paises.sort((a,b) => a.poblacion - b.poblacion)
+//                 return menorAmayor.slice(primerPais,ultimoPais)
+//             }
+//             if(filtro.filtro === "E"){
+//                 const mayorAmenor = paises.sort((a,b) => b.poblacion - a.poblacion)
+//                 return mayorAmenor.slice(primerPais,ultimoPais)
+//             }
+//             if(filtro.filtro === "F"){
+//                 const america = paises.filter(p => p.continente === "Americas")
+//                 return america.slice(primerPais,ultimoPais)
+//             }
+//             if(filtro.filtro === "G"){
+//                 const europa = paises.filter(p => p.continente === "Europe")
+//                 return europa.slice(primerPais,ultimoPais)
+//             }
+//             if(filtro.filtro === "H"){
+//                 const africa = paises.filter(p => p.continente === "Africa")
+//                 return africa.slice(primerPais,ultimoPais)
+//             }
+//             if(filtro.filtro === "I"){
+//                 const asia = paises.filter(p => p.continente === "Asia")
+//                 return asia.slice(primerPais,ultimoPais)
+//             }
+//             if(filtro.filtro === "J"){
+//                 const oceania = paises.filter(p => p.continente === "Oceania")
+//                 return oceania.slice(primerPais,ultimoPais)
+//             }
+//             if(filtro.filtro === "K"){
+//                 const antartida = paises.filter(p => p.continente === "Antarctic")
+//                 return antartida.slice(primerPais,ultimoPais)
+//             }           
            
-                 const paisesnormales = paises.slice(primerPais,ultimoPais)
-                 return paisesnormales  
-     } 
-             if(filtro.filtro.length > 1){
-                const porActividades = paises.filter(p => filtro.filtro.includes(p.id))
-                return porActividades
-     }
-     const paisesnormales = paises.slice(primerPais,ultimoPais)
-     return paisesnormales  
- }
+//                  const paisesnormales = paises.slice(primerPais,ultimoPais)
+//                  return paisesnormales  
+//      } 
+//              if(filtro.filtro.length > 1){
+//                 const porActividades = paises.filter(p => filtro.filtro.includes(p.id))
+//                 return porActividades
+//      }
+//      const paisesnormales = paises.slice(primerPais,ultimoPais)
+//      return paisesnormales  
+  }
+
+
+  const filtrarOrdenamiento = ( paises ) => {
+    if(datosBusqueda.ordenamiento){
+        if(datosBusqueda.ordenamiento === "A_Z") {
+            const a_z = paises.sort((a,b) => a.nombre.localeCompare(b.nombre, 'en') ) 
+            return a_z//.slice(primerPais,ultimoPais)
+        }
+        if(datosBusqueda.ordenamiento === "Z_A"){
+            const z_a = paises.sort((a,b) => b.nombre.localeCompare(a.nombre, 'en'))
+            return z_a//.slice(primerPais,ultimoPais)
+        }
+        if(datosBusqueda.ordenamiento === "menorAmayor"){
+            const menorAmayor = paises.sort((a,b) => a.poblacion - b.poblacion)
+            return menorAmayor//.slice(primerPais,ultimoPais)
+        }
+        if(datosBusqueda.ordenamiento === "mayorAmenor"){
+            const menorAmayor = paises.sort((a,b) => b.poblacion - a.poblacion)
+            return menorAmayor//.slice(primerPais,ultimoPais)
+        }
+    } 
+    return paises
+  }
+
+  const filtrarContinente = (pais) => {
+    if (datosBusqueda.continente){
+        return pais.continente === datosBusqueda.continente
+    }
+    return pais
+  }
+
+  const filtrarActividades = (pais) => {
+    if(datosBusqueda.actividad){
+        return datosBusqueda.actividad.includes(pais.id) 
+    }
+    return pais
+  }
+
+  const filtrarBuscador = (pais) => {
+    if(buscador){
+        const resultado = pais.nombre.slice(0, buscador.length).toLowerCase() === buscador.toLowerCase().trim()
+        return resultado//.slice(primerPais,ultimoPais)
+    }
+    return pais
+  }
     
     
     // Cambiar de pagina
@@ -119,38 +175,38 @@ export const Cards = () => {
                         className='estiloinput' 
                         placeholder='Busca un Pais...'
                         type="text"
+                        name='buscador'
                         value={buscador}
                         onChange={e => setBuscador(e.target.value)}
                         />
                 </div>
                 <div className='estilodiv'>  
                     <label className='estilolabel'>Filtros</label>
-                        <select name="filtro" onChange={e => ordenar(e)} className="estiloinput">
+                        <select id='ordenamiento' name="ordenamiento" onChange={e => ordenar(e)} className="estiloinput">
                         <option value="">Selecciona un Filtro</option>
-                        <option value="A">Api form</option>
-                        <option value="B" >A-Z</option>
-                        <option value="C" >Z-A</option>
-                        <option value="D">Menor Poblacion</option>
-                        <option value="E">Mayor Poblacion</option>
+                        <option value="A_Z" >A-Z</option>
+                        <option value="Z_A" >Z-A</option>
+                        <option value="menorAmayor">Menor Poblacion</option>
+                        <option value="mayorAmenor">Mayor Poblacion</option>
                     </select>
                 </div>
                 <div className='estilodiv'> 
                     <label className='estilolabel'>Continente</label>
-                        <select name="filtro" onChange={e => ordenar(e)} className="estiloinput">
+                        <select id='continente' name="continente" onChange={e => ordenar(e)} className="estiloinput">
                         <option value="">Selecciona un Continente</option>    
-                        <option value="F">America</option>
-                        <option value="G">Europa</option>
-                        <option value="H">Africa</option>
-                        <option value="I">Asia</option>
-                        <option value="J">Oceania</option>
-                        <option value="K">Antartida</option>
+                        <option value="Americas">America</option>
+                        <option value="Europe">Europa</option>
+                        <option value="Africa">Africa</option>
+                        <option value="Asia">Asia</option>
+                        <option value="Oceania">Oceania</option>
+                        <option value="Antarctic">Antartida</option>
                         </select> 
                 </div>
                 <div className='estilodiv'> 
                     <label className='estilolabel'>Actividades</label>
-                        <select name="filtro" onChange={e => ordenar(e)} className="estiloinput">
-                            <option>Selecciona una Actividad</option>
-                           {actividades.length == 0? <option>No hay actividades</option> : actividades?.map(a => (
+                        <select id='actividad' name="actividad" onChange={e => ordenar(e)} className="estiloinput">
+                            <option value="">Selecciona una Actividad</option>
+                           {actividades.length == 0? <option value="">No hay actividades</option> : actividades?.map(a => (
                             <option value={a.fusion}>{a.actividad.nombre}</option>
                            ))} 
                         </select> 
@@ -168,7 +224,7 @@ export const Cards = () => {
             <Paginacion paisesXPagina={paisesXPagina} paisesTotales={paises.length} paginar={paginar}/>
             </div>
             <div className= "paises"> 
-                {cargador? <Spiner/> : mapeoDePaises()?.length == 0? <h1 className='no-existe'>No Existe ese Pais</h1> : mapeoDePaises()?.map(pais => (
+                {cargador? <Spiner/> : filtroDePaises()?.length == 0? <h1 className='no-existe'>No Existe ese Pais</h1> : filtroDePaises()?.map(pais => (
                 <div key={pais.id} className="individual">
                     <Pais 
                     id ={pais.id}
